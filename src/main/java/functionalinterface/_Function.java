40bfe77e38ad5da36c4e4464f02d882adeae018c
Lalama1982 <lalamaesp@gmail.com>
2020-03-29 23:01:43 +1100

    Bi-Functions

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -1,5 +1,6 @@
 package functionalinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
@@ -11,12 +12,42 @@ public class _Function {
         int increment2 = incrementByOneFunction.apply(1);
         System.out.println("via utilFunction calling .... [incr from 1] " + increment2);
 
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println("via utilFunction calling .... [multiply by 10] " + multiply);
+
+        /*
+         * this was is also possible! Function<Integer, Integer>
+         * addBy1AndThenMultiplyBy10 =
+         * incrementByOneFunction.andThen(multiplyBy10Function);
+         * System.out.println("via utilFunction calling .... [use of andThen] " +
+         * addBy1AndThenMultiplyBy10.apply(3));
+         */
+
+        int vAndThen = addBy1AndThenMultiplyBy10.apply(multiply);
+        System.out.println("via utilFunction calling .... [use of andThen] " + vAndThen);
+
+        // Use of bi-functions
+        System.out.println("via std bi-function replicate "+incrementByOneAndMultiply(4,10) );
+        System.out.println("via std bi-function replicate [use of util] "+incrementByOneAndMultiplyBiFunction.apply(4,10) );
+
     }
 
+    // Use of b-function
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction = (numberToIncrementByOne,
+            numberToMultiplyBy) -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+        return (number + 1) * numToMultiplyBy;
+    }
+
+    // Std function
     // Use of util.function, package
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
 
-    // Std function
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+    static Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
+
     static int incrementByOneStdFunction(int number) {
         return number + 1;
     }
